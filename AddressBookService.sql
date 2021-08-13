44e8cfb3ba3ece57df24f0e84245677d266391e8
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-13 07:16:35 +0530

    Create AddressBookService.sql

---
+++ AddressBookService.sql
@@ -0,0 +1,35 @@
+create database Address_book_service
+
+select * from sys.databases
+
+use Address_book_service
+
+create table employee_payroll
+( 
+first_name varchar(25),
+last_name varchar(30),
+address varchar(100),
+city varchar(25),
+state varchar(30),
+zip varchar(6),
+Phone_number varchar(15),
+email varchar(50))
+
+select * from employee_payroll
+
+insert into employee_Payroll values
+(
+'Arya','Khadakkar','pratapNagar','Nagpur','Maharashtra',440022,9922861753,'aryakhadakkar03@gmail.com'),
+('Anjali','khadakkar','pratapNagar','Nagpur','Maharashtra',440022,9922868753,'anajal@gmail.com'),
+('Abhijit','Khadakkar','pratapNagar','Nagpur','Maharashtra',440022,9912000001,'raki@gmail.com'),
+('Omkar','Khadakkar','pratapNagar','Nagpur','Maharashtra',440022,9141253625,'omkar9587@gmail.com');
+
+update employee_payroll set zip = 573115 where first_name = 'Abhijit'
+
+Delete from employee_payroll where first_name = 'Abhjiit'
+
+select * from employee_payroll where city = 'Nagpur' and  state = 'Maharashtra'
+
+select COUNT(first_name) from employee_payroll where city = 'Abhjiit' and state = 'Maharashtra'
+
+select * from employee_payroll ORDER BY first_name,city
