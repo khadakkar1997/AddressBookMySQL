360138c6157d17061a04a52bd882609b547194e0
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-13 07:31:45 +0530

    Update AddressBookService.sql

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,1 +1,19 @@
 
+create database Address_book_service
+
+select * from sys.databases
+
+use Address_book_service
+
+create table employee_payroll
+( 
+first_name varchar(25),
+last_name varchar(30),
+address varchar(100),
+city varchar(25),
+state varchar(30),
+zip varchar(6),
+Phone_number varchar(15),
+email varchar(50))
+
+select * from employee_payroll
